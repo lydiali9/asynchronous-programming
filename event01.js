49d59582464a6b4acceeeb1f6aaed3d258557300
Lynn Li <648823000@qq.com>
2017-12-11 00:02:31 -0800

    [feature]: add event function

---
+++ event01.js
@@ -0,0 +1,15 @@
+// 网页爬取程序
+var http = require('http');
+var url = 'http://www.baidu.com';
+
+http.get(url, function(res) {
+  res.setEncoding('utf-8');
+
+  res.on('data', function(data) {
+    console.log(data);
+  });
+
+  res.on('end', function() {
+    console.log('game over!');
+  });
+});
---
+++ event02.js
@@ -0,0 +1,17 @@
+// 自定义事情发生器
+var EventEmitter = require('events').EventEmitter;
+var eventObj = new EventEmitter();
+
+eventObj.on('done', function(id) {
+  console.log('event done with id', id);
+});
+
+console.log('begin');
+
+setTimeout(function() {
+  eventObj.emit('done', 1);
+}, 1000);
+
+eventObj.emit('done', 2);
+
+console.log('end');
